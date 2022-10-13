8717d2b5f59c82db9ac376dfa34781b7c1e53ee9
Alson <alson.chang@nzxt.com>
2022-10-13 14:28:20 +0800

    Add my-nginx resource in terraform

--- terraform/demo-app.tf
+++ terraform/demo-app.tf
@@ -307,3 +307,15 @@ resource "kubernetes_service" "mysqldb" {
   }
 }
 
+data "kubectl_file_documents" "my-nginx-app" {
+  content = file("../manifests/argocd/my-nginx-app.yaml")
+}
+
+resource "kubectl_manifest" "my-nginx-app" {
+  depends_on = [
+    kubectl_manifest.argocd,
+  ]
+  count              = length(data.kubectl_file_documents.my-nginx-app.documents)
+  yaml_body          = element(data.kubectl_file_documents.my-nginx-app.documents, count.index)
+  override_namespace = "argocd"
+}
